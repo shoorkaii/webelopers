58273fe78505b77afe164d5794e486931f77fe7e
Reza <shoorkaii@gmail.com>
2017-10-25 16:50:49 +0330

    home view added

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from account.views import LoginView
+from account.views import LoginView, HomeView
 
 urlpatterns = [
-    url(r'^$', LoginView.as_view(), name='login')
+    url(r'^$', LoginView.as_view(), name='login'),
+    url(r'^home/$', HomeView.as_view(), name='home'),
 ]
--- backend/account/views.py
+++ backend/account/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.contrib.auth import login
 from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
 
 
@@ -9,7 +10,15 @@ class LoginView(FormView):
     form_class = AuthenticationForm
     template_name = 'login.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        request.session.set_test_cookie()
+        return super(LoginView, self).dispatch(request, *args, **kwargs)
+
     def form_valid(self, form):
-        login(form.request, form.get_user())
+        login(self.request, form.get_user())
 
         return super(LoginView, self).form_valid(form)
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'

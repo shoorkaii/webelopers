05c3e669dd334232a63b612525a6c7c52c85a5d2
Reza <shoorkaii@gmail.com>
2017-10-25 14:49:44 +0330

    login view added

---
+++ backend/account/views.py
@@ -0,0 +1,14 @@
+# Create your views here.
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic.edit import FormView
+
+
+class LoginView(FormView):
+    success_url = '/home/'
+    form_class = AuthenticationForm
+
+    def form_valid(self, form):
+        login(form.request, form.get_user())
+
+        return super(LoginView, self).form_valid(form)

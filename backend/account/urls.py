376d42af2952f570de6301d5a7c551187a19cf8c
Reza <shoorkaii@gmail.com>
2017-10-25 17:03:28 +0330

    login required added

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from account.views import LoginView, HomeView
+from account.views import LoginView, HomeView, LogoutView
 
 urlpatterns = [
     url(r'^$', LoginView.as_view(), name='login'),
     url(r'^home/$', HomeView.as_view(), name='home'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
 ]
--- backend/account/views.py
+++ backend/account/views.py
@@ -1,7 +1,9 @@
 # Create your views here.
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
-from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.base import TemplateView, RedirectView
 from django.views.generic.edit import FormView
 
 
@@ -20,5 +22,13 @@ class LoginView(FormView):
         return super(LoginView, self).form_valid(form)
 
 
-class HomeView(TemplateView):
+class LogoutView(RedirectView):
+    url = '/'
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return super(LogoutView, self).get(request, *args, **kwargs)
+
+
+class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'home.html'

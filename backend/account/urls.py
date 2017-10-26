a165df83203543b16fec06817352b187df48c67b
Reza <shoorkaii@gmail.com>
2017-10-26 15:37:00 +0330

    list view added

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from account.views import LoginView, HomeView, LogoutView, UserInfoView, BetterUserInfoView
+from account.views import LoginView, HomeView, LogoutView, UserInfoView, BetterUserInfoView, ShowUserInfoView
 
 urlpatterns = [
     url(r'^$', LoginView.as_view(), name='login'),
     url(r'^home/$', HomeView.as_view(), name='home'),
-    url(r'^user-info/$', UserInfoView.as_view(), name='user-info'),
+    url(r'^user-info/$', BetterUserInfoView.as_view(), name='user-info'),
+    url(r'^all/$', ShowUserInfoView.as_view(), name='all'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
 ]
--- backend/account/views.py
+++ backend/account/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView, RedirectView
 from django.views.generic.edit import FormView, UpdateView
+from django.views.generic.list import ListView
 
 from account.forms.user_forms import UserInfoForm
 from account.models import UserInfo
@@ -44,7 +45,7 @@ class UserInfoView(LoginRequiredMixin, FormView):
     def get_initial(self):
         initial = super(UserInfoView, self).get_initial()
         if self.request.user.additionals.count() > 0:
-            user_info = self.request.user.additionals.all()[1]
+            user_info = self.request.user.additionals.all()[0]
             initial['phone_number'] = user_info.phone_number
             initial['address'] = user_info.address
             initial['profile_picture'] = user_info.profile_picture
@@ -52,18 +53,36 @@ class UserInfoView(LoginRequiredMixin, FormView):
 
     def form_valid(self, form):
         if self.request.user.additionals.count() > 0:
-            user_info = self.request.user.additionals.all()[1]
+            user_info = self.request.user.additionals.all()[0]
+            user_info.phone_number = form.data['phone_number']
+            user_info.save()
+        else:
+            user_info = UserInfo()
+            user_info.user = self.request.user
             user_info.phone_number = form.data['phone_number']
             user_info.save()
         return super(UserInfoView, self).form_valid(form)
 
 
+# TODO: see CreateView later
 class BetterUserInfoView(LoginRequiredMixin, UpdateView):
     model = UserInfo
     form_class = UserInfoForm
     template_name = 'form.html'
+    success_url = '/user-info/'
 
     def get_object(self, queryset=None):
         if self.request.user.additionals.count() > 0:
-            return self.request.user.additionals.all()[1]
+            return self.request.user.additionals.all()[0]
         return None
+
+
+# TODO: see DetailView later
+class ShowUserInfoView(LoginRequiredMixin, ListView):
+    model = UserInfo
+    template_name = 'all.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ShowUserInfoView, self).get_context_data(**kwargs)
+        print(context)
+        return context
